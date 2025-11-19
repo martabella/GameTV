7f14acb6115c05904ff56dfabb5660555d715ea5
martabella <mbella@lasalle.cat>
2025-11-19 15:37:15 +0100

    Initial Commit

---
+++ GameTV/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GameTV
+//
+//  Created by Marta Bella on 19/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, swift!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameTV/GameTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameTVApp.swift
+//  GameTV
+//
+//  Created by Marta Bella on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GameTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
