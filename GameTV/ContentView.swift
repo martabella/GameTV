ef64666bcb77fc852a543328ed2c107249458d24
martabella <mbella@lasalle.cat>
2025-11-19 16:23:35 +0100

    Adding views to contentView

--- GameTV/ContentView.swift
+++ GameTV/ContentView.swift
@@ -1,22 +1,36 @@
-//
-//  ContentView.swift
-//  GameTV
-//
-//  Created by Marta Bella on 19/11/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
 
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, swift!")
-        }
-        .padding()
+            Text("🎯🎯🎯").font(.largeTitle)
+            Text("89")
+                .font(.largeTitle)
+                .kerning(-1)
+                .fontWeight(.bold)
+            Slider(
+                value: .constant(90),
+                    in: 1...100,
+                    step: 1
+                ) {
+                    Text("Slider")
+                } minimumValueLabel: {
+                    Text("1").fontWeight(.bold)
+                } maximumValueLabel: {
+                    Text("100").fontWeight(.bold)
+                }
+            Button("TRY"){
+                print("try tapped")
+            }.padding()
+                .font(.title3)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(21)
+                
+            
+        }.padding()
+        
     }
 }
 
